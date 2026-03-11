50ba2953f13ac5055cdb6eeada0fdbc725a36822
Dhaniel Lofamia <dhanieeel0907@gmail.com>
2026-03-11 11:50:09 +0800

    3/11/26 folder structure

--- frontend/src/components/cards/ProductCard.tsx
+++ frontend/src/components/cards/ProductCard.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import type { Product } from "../../data/products";
+import type { Product } from "../../../data/products";
 
 type ProductCardProps = {
   product: Product;
