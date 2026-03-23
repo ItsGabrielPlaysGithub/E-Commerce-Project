ec3ef0d2d0a9b7bfd42694722d0411321df9613e
Lance Ivan Reyes Listana - Synchores <264211553+Lance-Synchores@users.noreply.github.com>
2026-03-23 13:37:33 +0800

    test pull

--- frontend/src/features/admin/products/hooks/use-products.ts
+++
@@ -1,7 +0,0 @@
-import { useQuery } from "@apollo/client/react";
-import { GET_PRODUCTS } from "../services/query";
-import { GetProductsQuery } from "@/gql/graphql";
-
-export const useProducts = () => {
-    return useQuery<GetProductsQuery>(GET_PRODUCTS);
-};
